cc77fe2ed35e7cd1cb2c10312d22d13c06b55ace
Ranjani Murali <43966472+rmurali9@users.noreply.github.com>
2018-12-09 15:41:39 -0500

    Update __init__.py

--- maml_rl/policies/__init__.py
+++ maml_rl/policies/__init__.py
@@ -2,3 +2,4 @@ from .categorical_mlp import CategoricalMLPPolicy
 from .normal_mlp import NormalMLPPolicy
 from .critic_mlp import CriticFunction
 from .policy import Policy
+from .normal_mlp_a2c import NormalMLPPolicyA2C
